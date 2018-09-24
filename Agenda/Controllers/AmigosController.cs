79b47830fac52975d8979f63b3d8c58f86413e9d
William Ballesteros Blanco <ballesteroswilliam@avaya.com>
2018-09-23 22:42:10 -0500

    Add database.sql

--- Agenda/Controllers/AmigosController.cs
+++ Agenda/Controllers/AmigosController.cs
@@ -1,11 +1,9 @@
-﻿using System;
+﻿using Agenda.Models;
+using Agenda.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Agenda.Models;
-using Agenda.Repository;
 
 namespace Agenda.Controllers
 {
